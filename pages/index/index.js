0fb20a8667817ea6c6a4b15328cb17e77ee427de
jayman99 <jayman.mu@gmail.com>
2020-08-16 22:09:30 +0800

    注册小程序和页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,65 @@
 // pages/index/index.js
+// getApp() 获取App()产生的示例对象
+// const app = getApp()
+
+// const name = app.globalData.name;
+// const age = app.globalData.age;
+
+// 注册一个页面，页面有自己的生命周期函数
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // -----------2. 初始化数据-------------
   data: {
-    
+    message: "QQ音乐",
+    list: []
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  // -----------1. 监听页面的生命周期-------------
+  // 页面被加载出来时
+  onLoad() {
+    console.log('onLoad')
+    // 发送请求
+    wx.request({
+      // qq音乐热门推荐
+      url: 'https://api.qq.jsososo.com/recommend/playlist',
+      success: (res) => {
+        console.log(res)
+        const data = res.data.data.list;
+        console.log(data)
+        this.setData({
+          list: data
+        })
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  // 页面显示出来时
+  onShow() {
+    console.log('onShow')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  // 页面初次渲染完成时
+  onReady() {
+    console.log('onReady')
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
+  // 页面被隐藏时
+  onHide() {
+    console.log('onHide')
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
+  onUnload() {
+    console.log('onUnload')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
+  // -----------3. 监听wxml中相关的一些事件-------------
+  handleUserInfo(event) {
+    console.log(event)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
+  handleViewClick() {
+    console.log("QQ音乐被点击了")
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  // -----------4. 监听其他事件-------------
+  onPageScroll(obj) {
+    console.log("页面被滚动了")
+  },
+  onReachBottom() {
+    console.log("页面到底部了")
+  },
+  onPullDownRefresh() {
+    console.log("页面下拉刷新")
   }
 })
