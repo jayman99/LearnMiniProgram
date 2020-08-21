afac9afb0e287424ba6642f106423d1bcd2cb71a
jayman99 <jayman.mu@gmail.com>
2020-08-21 17:08:15 +0800

    组件化开发

---
+++ components/my-slot/my-slot.js
@@ -0,0 +1,9 @@
+// components/my-slot/my-slot.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,35 @@
 // pages/index/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    
+    counter: 0,
+    isShow: true
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  handleIncrement(event) {
+    console.log("-----------------", event)
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+  handleTabClick(event) {
+    console.log(event)
   },
+  handleIncrementCpn() {
+    console.log('--------------------')
+    // 目的是修改组件的数据
+    // 1.获取组件对象
+    const my_sel = this.selectComponent("#sel_id")
+    // console.log(my_sel)
+    // 2.通过setData()修改组件中的数据(一般不采用)
+    // my_sel.setData({
+    //   counter: my_sel.data.counter + 20
+    // })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+    // 3.通过组件方法暴露一个对象，再修改数据
+    my_sel.incrementCounter(10)
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  handleChangeShow() {
+    this.setData({
+      isShow: !this.data.isShow
+    })
   }
 })
