2381d6670d0506e5d2c6c57e0eb5f59457773f01
jayman99 <jayman.mu@gmail.com>
2020-08-22 16:13:31 +0800

    弹窗和分享

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,66 @@
 // pages/index/index.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
+  handleShowToast() {
+    wx.showToast({
+      title: '加载中',
+      duration: 3000,
+      icon: 'loading',
+      // image: '/assets/icon/icon.png'
+      mask: true,
+      success: () => {
+        console.log('展示成功')
+      },
+      fail: () => {
+        console.log('展示失败')
+      },
+      complete: () => {
+        console.log('展示完成')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
+  handleShowModal() {
+    wx.showModal({
+      title: 'Modal的标题',
+      content: 'Modal的内容',
+      // showCancel: false,
+      cancelText: '退出',
+      cancelColor: 'red',
+      success: (res) => {
+        console.log(res)
+        if(res.cancel) {
+          console.log('用户点击了取消按钮')
+        }
+        if(res.confirm) {
+          console.log('用户点击了确定按钮')
+        }
+      }
+    })
   },
+  handleShowLoading() {
+    wx.showLoading({
+      title: '加载中',
+      mask: true
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
+    setTimeout(() => {
+      // 必须手动hideLoading才会让loading消失
+      wx.hideLoading()
+    }, 1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  handleShowActionSheet() {
+    wx.showActionSheet({
+      itemList: ['相册', '拍照'],
+      itemColor: 'red',
+      success: (res) => {
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: (options) => {
+    return {
+      title: '你好啊！',
+      path: '/pages/index/index',
+      imageUrl: 'http://r.photo.store.qq.com/psb?/V11IWYp61c6zKx/rQHWdB5aA*QVHwGzg3ecmMxAnViImW5GsxPqKki8mUs!/r/dD0BAAAAAAAA'
+    }
   }
 })
